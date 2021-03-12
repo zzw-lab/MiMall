3105308a0ef35c2e2e32a83f52a3d67da81e7288
“zzw-lab” <1159107152@qq.com>
2021-03-12 19:02:40 +0800

    5-10首页轮播菜单实现

--- src/App.vue
+++ src/App.vue
@@ -14,44 +14,15 @@ export default {
   },
   data(){
     return{
-     res:{}
+    
     }
   },
   mounted(){
-    // let url= "/pay/cartorder";
-    // jsonp(url,()=>{
-
-    // })
-
-    // storage.setItem('user',{a:1});
-    //storage.setItem('abc',{a:1},'user');
-    // storage.clear('a','user');
-
-    //Mock设置方法一：本地加载请求静态json文件
-    // this.axios.get('/mock/user/login.json').then((res)=>{
-    //   this.res = res;
-    // });
-
-    //Mock设置方法二：通过easy-mock平台实现数据mock
-    // this.axios.get('/user/login').then((res)=>{
-    //   this.res = res;
-    // });
-
-    //Mock设置方法三：本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res)=>{
-      this.res = res;
-    });
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,58 @@
 <template>
-  <div>nav-footer</div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="javascript:;" target="_blank">河畔一角主页</a><span>|</span>
+      <a href="javascript:;" target="_blank">Vue全栈课程</a><span>|</span>
+      <a href="javascript:;" target="_blank">React全家桶课程</a><span>|</span>
+      <a href="javascript:;" target="_blank">微信支付专项课程（H5+小程序+Node+Mongo）</a>
+    </div>
+    <div class="copyright">
+      © mi.com 京ICP证110507号 <span class="domain"> 京公网安备11010802020134号</span> 京网文[2020]0276-042号
+    </div>
+  </div>
 </template>
 <script>
   export default{
     name:'nav-footer'
   }
 </script>
+<style lang="scss" scoped>
+  @import './../assets/scss/config.scss';
+  .footer{
+    font-size: 12px;
+    height: 234px;
+    border-top:4px solid $colorA;
+    background-color: #333;
+    color:#999;
+    font-size: 16px;
+    text-align: center;
+    .footer-logo{
+      margin-top:46px;
+      margin-bottom:31px;
+      img{
+        width: 53px;
+        height: 36px;
+        margin-bottom: 13px;
+      }
+    }
+    .footer-link{
+      a{
+        color:#999;
+        display: inline-block;
+      }
+      span{
+        margin:0 10px;
+      }
+      margin-bottom: 13px;
+    }
+    .copyright{
+      .domain{
+        color:$colorA;
+      }
+    }
+  }
+</style>
